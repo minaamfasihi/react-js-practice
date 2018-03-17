74b9c09c566ff84e8305d5882561c8887a56984d
minaamfasihi <minaam.fasihi@gmail.com>
2018-03-18 03:36:22 +0500

    Understanding of asynchronous setState and how to run something only after setState is complete

---
+++ test-app-for-set-state/src/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Counter extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            count: 0
+        }
+
+        this.incrementCounter = this.incrementCounter.bind(this);
+    }
+
+    incrementCounter() {
+        this.setState({
+            count: this.state.count + 1
+        }, () => {
+            console.log(this.state.count); // always occurs after this.setState is complete
+        });
+        console.log(this.state.count);
+
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            }
+        });
+    }
+
+    render() {
+        const { count } = this.state;
+        return(
+            <section>
+                {count}
+                <button onClick={this.incrementCounter}>
+                    Add
+                </button>
+            </section>
+        )
+    }
+}
+
+ReactDOM.render(<Counter />, document.getElementById("root"));
+
